79c9ca11cb92f4ae23fd31214eadc483ad2836b8
Apiseg Chainarong <apiseg.chainarong@outlook.com>
2022-09-18 22:01:43 +0200

    adding readme for settings which needs to be set manually

--- dependencies/brew_apps.sh
+++ dependencies/brew_apps.sh
@@ -1,10 +1,12 @@
 brew install kubernetes-cli \
   helm \
   node \
+  neovim
 
 brew install --cask docker \
   insomnia \
   anaconda \
   sourcetree \ 
   cyberduck \
-  lens
+  lens \
+  visual-studio-code 
