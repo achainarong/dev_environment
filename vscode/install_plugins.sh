38b7e609e4ca7154d1b6d9d5c39197e9792c2f46
Apiseg Chainarong <apiseg.chainarong@outlook.com>
2022-09-18 23:19:43 +0200

    adding vs code plugins

---
+++ vscode/install_plugins.sh
@@ -0,0 +1,15 @@
+code --install-extension GitHub.github-vscode-theme
+code --install-extension esbenp.prettier-vscode
+code --install-extension bmalehorn.shell-syntax
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension redhat.ansible
+code --install-extension redhat.vscode-yaml
+code --install-extension redhat.java
+code --install-extension jeff-hykin.better-dockerfile-syntax
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag 
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension VisualStudioExptTeam.vscodeintellicode
