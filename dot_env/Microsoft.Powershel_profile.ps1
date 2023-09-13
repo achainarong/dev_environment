81ba16f6ab386ecc36f30258d72704552600b3f3
achainarong <88964426+achainarong@users.noreply.github.com>
2023-09-13 11:51:35 +0200

    Update Microsoft.Powershel_profile.ps1

--- dot_env/Microsoft.Powershel_profile.ps1
+++ dot_env/Microsoft.Powershel_profile.ps1
@@ -1,26 +1,44 @@
-oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/jandedobbeleer.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/M365.omp.json | Invoke-Expression
 
-Function git_status {git status}
-Function git_add {git add .}
-Function git_commit {git commit -m}
-Function git_push {git push}
+Function gs {git status}
+Function add {git add .}
+Function commit 
+{
+    git commit -m $args[0]
+}
 
-Function cd_nvim {Set-Location -Path ~\AppData\Local\nvim}
+Function nconf {Set-Location -Path ~\AppData\Local\nvim}
 Function cd_back {cd ..}
 Function cd_back_twice {cd ..\..}
-Function nvim_tree {nvim .}
+Function n {nvim .}
+Function home { Set-Location -Path  ~\ }
 
-Set-Alias -Name list -Value Get-ChildItem
-Set-Alias -Name gs -Value git_status
-Set-Alias -Name add -Value git_add
-Set-Alias -Name commit -Value git_commit
-Set-Alias -Name push -Value git_push
+Function expand {
+    param (
+        [Parameter(Mandatory=$true)]
+        [Alias("d")]
+        [string]$sourceDirectory
+    )
 
-Set-Alias -Name nconf -Value cd_nvim
+    if (-not (Test-Path $sourceDirectory)) {
+        Write-Error "Source directory does not exist: $sourceDirectory"
+        return
+    }
+
+    # Loop over each ZIP file in the directory
+    Get-ChildItem -Path $sourceDirectory -Filter *.zip | ForEach-Object {
+        # Define the destination directory (same as source directory in this example)
+        $destinationDirectory = $_.FullName.Replace('.zip', '')
+
+        # Expand the archive
+        Expand-Archive -Path $_.FullName -DestinationPath $destinationDirectory
+        Write-Output "Unzipped $($_.Name) to $destinationDirectory"
+    }
+}
+
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name k -Value kubectl
 Set-Alias -Name terra -Value terraform
 Set-Alias -Name py -Value python
 Set-Alias -Name .. -Value cd_back
 Set-Alias -Name ... -Value cd_back_twice
-Set-Alias -Name n. -Value nvim_tree
